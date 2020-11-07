1927d3e9251af4b17b82940eb377a17feb908cea
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-07 23:07:01 +0530

    update

--- tours/src/App.js
+++ tours/src/App.js
@@ -6,7 +6,12 @@ const url = "https://course-api.com/react-tours-project";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [tours, settours] = useState([]);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   const fetchTours = async () => {
     setLoading(true);
@@ -15,7 +20,7 @@ function App() {
       const response = await fetch(url);
       const tours = await response.json();
       setLoading(false);
-      settours(tours);
+      setTours(tours);
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -33,9 +38,22 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
