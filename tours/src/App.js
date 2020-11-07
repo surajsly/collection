f88236ddfedf8591b37cce6b7e29b9231782c20b
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-07 22:24:50 +0530

    app update

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,11 +1,41 @@
 import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, settours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      settours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
     <main>
-      <h3>sss</h3>
+      <Tours tours={tours} />
     </main>
   );
 }
--- tours/src/Loading.js
+++ tours/src/Loading.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Loading = () => {
   return (
     <div className="loading">
-      <h1>loading...</h1>
+      <h1>Loading...</h1>
     </div>
   );
 };
