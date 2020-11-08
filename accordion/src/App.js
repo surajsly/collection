0f67e6c3f7749a8a3a38d4a5e8323662341c4aed
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-08 22:46:10 +0530

    question

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
     <main>
-      <h2>accordion project setup</h2>
+      <div className="container">
+        <h3>Question and answer about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
     </main>
   );
 }
