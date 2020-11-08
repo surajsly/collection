c526b7983bc84bc6498e54cf0ee2d0da31c1c376
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-08 21:24:17 +0530

    review component complete

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
+import React from "react";
+import Review from "./Review";
 
 function App() {
   return (
-    <div className="App">
-      <h2>suraj</h2>
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
